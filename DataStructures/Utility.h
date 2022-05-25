d61ce0854868fb6a7b658d4a50895e960c91a9ef
kubsnn <51138889+kubsnn@users.noreply.github.com>
2022-05-26 00:31:50 +0200

    forward, array comparison

--- DataStructures/Utility.h
+++ DataStructures/Utility.h
@@ -12,7 +12,7 @@ typedef int              ptrdiff_t;
 typedef int              intptr_t;
 #endif
 
-template<class _Ty1, class _Ty2>
+template <class _Ty1, class _Ty2>
 struct Pair
 {
 	_Ty1 first;
@@ -24,7 +24,7 @@ struct Pair
 	}
 };
 
-template<class _Ty1, class _Ty2>
+template <class _Ty1, class _Ty2>
 struct Hash<Pair<_Ty1, _Ty2>>
 {
 	size_t operator()(const Pair<_Ty1, _Ty2>& _Pair) {
@@ -34,25 +34,55 @@ struct Hash<Pair<_Ty1, _Ty2>>
 	}
 };
 
-template<class _Ty>
+template <class _Ty>
 constexpr const _Ty& max(const _Ty& _Left, const _Ty& _Right)
 {
 	return _Left < _Right ? _Right : _Left;
 }
 
-template<class _Ty>
+template <class _Ty>
 constexpr const _Ty& min(const _Ty& _Left, const _Ty& _Right)
 {
 	return _Left > _Right ? _Right : _Left;
 }
 
-template<class _Ty>
-inline _Ty&& move(_Ty& _Arg)
+template <class _Ty>
+struct remove_reference {
+	using type = _Ty;
+};
+
+template <class _Ty>
+struct remove_reference<_Ty&> {
+	using type = _Ty;
+};
+
+template <class _Ty>
+struct remove_reference<_Ty&&> {
+	using type = _Ty;
+};
+
+template <class _Ty>
+using remove_reference_t = typename remove_reference<_Ty>::type;
+
+template <class _Ty>
+inline constexpr _Ty&& forward(remove_reference_t<_Ty>& _Arg) noexcept
 {
 	return static_cast<_Ty&&>(_Arg);
 }
 
-template<class _Ty>
+template <class _Ty>
+inline constexpr _Ty&& forward(remove_reference_t<_Ty>&& _Arg) noexcept
+{ 
+	return static_cast<_Ty&&>(_Arg);
+}
+
+template <class _Ty>
+inline constexpr remove_reference_t<_Ty>&& move(_Ty& _Arg) noexcept
+{
+	return static_cast<remove_reference_t<_Ty>&&>(_Arg);
+}
+
+template <class _Ty>
 inline void swap(_Ty& _Left, _Ty& _Right)
 {
 	_Ty _Tmp(move(_Right));
@@ -60,7 +90,7 @@ inline void swap(_Ty& _Left, _Ty& _Right)
 	_Left = move(_Tmp);
 }
 
-template<class _FwdIt, class _Ty>
+template <class _FwdIt, class _Ty>
 inline constexpr void fill(const _FwdIt _First, const _FwdIt _Last, const _Ty& _Val)
 {
 	auto _UFirst = const_cast<_FwdIt>(_First);
@@ -69,7 +99,7 @@ inline constexpr void fill(const _FwdIt _First, const _FwdIt _Last, const _Ty& _
 	}
 }
 
-template<class _FwdIt, class _Ty>
+template <class _FwdIt, class _Ty>
 inline constexpr void fill_n(const _FwdIt _First, size_t _Count, const _Ty& _Val)
 {
 	auto _UFirst = const_cast<_FwdIt>(_First);
@@ -78,7 +108,7 @@ inline constexpr void fill_n(const _FwdIt _First, size_t _Count, const _Ty& _Val
 	}
 }
 
-template<class _InIt, class _OutIt>
+template <class _InIt, class _OutIt>
 inline void copy(_InIt _First, _InIt _Last, _OutIt _Dest)
 {
 	for (; _First != _Last; ++_First, ++_Dest) {
@@ -86,7 +116,7 @@ inline void copy(_InIt _First, _InIt _Last, _OutIt _Dest)
 	}
 }
 
-template<class _InIt, class _OutIt>
+template <class _InIt, class _OutIt>
 inline void move_mem(_InIt _Src, _OutIt _Dst, size_t _Count)
 {
 	if (_Src == _Dst) return;
@@ -105,10 +135,23 @@ inline void move_mem(_InIt _Src, _OutIt _Dst, size_t _Count)
 	}
 }
 
-template<class _Ty>
+template <class _Ty>
 inline constexpr _Ty nearest_bigger_power_of_2(const _Ty& _Val)
 {
 	int _Pow = 1;
 	while (_Pow < _Val) _Pow <<= 1;
 	return _Pow;
+}
+
+template <class InputIt1, class InputIt2>
+inline bool lexicographical_compare(const InputIt1 _First1, const InputIt1 _Last1,
+	const InputIt2 _First2, const InputIt2 _Last2)
+{
+	auto _UFirst1 = const_cast<InputIt1>(_First1);
+	auto _UFirst2 = const_cast<InputIt2>(_First2);
+	for (; (_UFirst1 != _Last1) && (_UFirst2 != _Last2); ++_UFirst1, ++_UFirst2) {
+		if (*_UFirst1 < *_UFirst2) return true;
+		if (*_UFirst2 < *_UFirst1) return false;
+	}
+	return (_UFirst1 == _Last1) && (_UFirst2 != _Last2);
 }
