fa60b1a8aedd50d074c1e44e730fa932e8978b9f
kubsnn <51138889+kubsnn@users.noreply.github.com>
2022-05-27 06:35:47 +0200

    Update main.cpp

--- DataStructures/main.cpp
+++ DataStructures/main.cpp
@@ -7,6 +7,7 @@
 #include "HashTable.h"
 #include "Vector.h"
 #include "Array.h"
+#include "Set.h"
 
 #include <iostream>
 #include <cstdlib>
@@ -20,6 +21,7 @@
 #include <array>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 
 #include <list>
 //#include <stack>
@@ -48,7 +50,7 @@ struct pair
 
 struct item {
     int x;
-    item(){ _STD cout << "CONSTRUCTOR" << _STD endl; }
+    item(){  }
     item(int x):x(x){ _STD cout << "INITIALIZE CONSTRUCTORE" << _STD endl; }
     item(const item& other):x(other.x){ _STD cout << "COPIED" << _STD endl; }
     item(item&& other)noexcept:x(move(other.x)){ _STD cout << "MOVED" << _STD endl; } 
@@ -60,13 +62,18 @@ struct item {
         x = move(other.x);
         return *this;
     }
-
+    bool operator==(const item& other) {
+        return x == other.x;
+    }
+    bool operator!=(const item& other) {
+        return x != other.x;
+    }
 };
 
 template <class _Container>
 void print_v(const _Container& v) {
     for (const auto& e : v) {
-        _STD cout << e << " ";
+        _STD cout << e.x << " ";
     }
     _STD cout << _STD endl;
 }
@@ -80,18 +87,50 @@ struct less<item>
     }
 };
 
+template<>
+struct Hash<item>
+{
+    size_t operator()(const item& _Item) 
+    {
+        return Hash<int>()(_Item.x);
+    }
+};
+
 int main()
 {
 
+    _STD ios_base::sync_with_stdio(false);
+
     srand(time(NULL));
 
-    auto v = range<int, Vector>(12, -3);
-    
+    HashTable<int, int> h;
+    h.insert(3, 5);
+    h.insert(6, 3);
+    h.insert(4, 2);
+
+    for (auto& [key, val] : h) {
+        _STD cout << key << " : " << val << _STD endl;
+    }
+
+    Vector<item> v;
+    v.emplace_back(3);
+    v.emplace_back(2);
+    v.emplace_back(5);
+    v.emplace_back(3);
+    v.emplace_back(7);
+
+
+    Set<item> s;
+    s.insert(v.begin(), v.end());
+
+    LinkedList<item> f;
+    f.append(s.begin(), s.end());
+
+    Vector<item> s2;
+    s2.append(f.begin(), f.end());
+    print_v(s2);
+    print_v(f);
     print_v(v);
-    
-   // sort(v.begin(), v.end(), greater{});
 
-
-    
 } 
 
